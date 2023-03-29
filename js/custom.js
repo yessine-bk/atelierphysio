41f2675b6ddbc4062db49fa0ad94e30ac575a770
bassem <bassem.grar@agyla.cloud>
2023-03-29 23:49:33 +0200

    initial commit of js files

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // REVIEWS CAROUSEL
+    $('.reviews-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 300,
+        smartSpeed: 500,
+        responsive:{
+          0:{
+            items:1,
+          },
+          768:{
+            items:2,
+            margin: 100,
+          },
+          1280:{
+            items:2,
+            margin: 100,
+          }
+        }
+    });
+
+    // Banner Carousel
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 1500,
+    })
+
+    // REVIEWS NAVIGATION
+    function ReviewsNavResize(){
+      $(".navbar").scrollspy({ offset: -94 });
+
+      var ReviewsOwlItem = $('.reviews-carousel .owl-item').width();
+
+      $('.reviews-carousel .owl-nav').css({'width' : (ReviewsOwlItem) + 'px'});
+    }
+
+    $(window).on("resize", ReviewsNavResize);
+    $(document).on("ready", ReviewsNavResize);
+
+    // HREF LINKS
+    $('a[href*="#"]').click(function (event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 74
+          }, 1000);
+        }
+      }
+    });
+    
+  })(window.jQuery);
+
+  //
+  var form = document.getElementById('myForm');
+  form.onsubmit = function(event){
+        var xhr = new XMLHttpRequest();
+        var formData = new FormData(form);
+
+        console.log("formData=====>",formData)
+        //open the request
+        xhr.open('POST','http://localhost:7000/tests/v1.0/form')
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        //send the form data
+        xhr.send(JSON.stringify(Object.fromEntries(formData)));
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                form.reset(); //reset form after AJAX success or do something else
+            }
+        }
+        //Fail the onsubmit to avoid page refresh.
+        return false; 
+    }
